d76f831b728e6d93e4e5551e90ca4eee4f6fb6d2
Andrew James <andrewpjames87@gmail.com>
2015-05-04 09:32:31 -0600

    functions almost complete

--- 00_functions/src.js
+++ 00_functions/src.js
@@ -1,10 +1,31 @@
 module.exports = {
 
   helloWorld: function(){
-    // fill me in
+    return 'Hello World';
   },
 
   hello: function(string){
-    // fill me in
+    return 'Hello '+ string +'';
+  },
+
+  shout: function(string){
+    return 'Hello '+ string.toUpperCase() +'';
+  },
+
+  whisper: function(string){
+    return 'Hello '+ string.toLowerCase() +'';
+  },
+
+  separate: function(string){
+    return string.split("");
+  },
+
+  reverseJoin: function(string){
+    return string.reverse().join();
+  },
+
+  reverseJoinCompact: function(string){
+    return string.reverse().join("");
   }
-}
+
+};
